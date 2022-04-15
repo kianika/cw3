9fc2a18fd064094603edf965bb29401feed4efb1
Abdolmohammad Mohammadian <mona_yaghoobi@yahoo.com>
2022-04-15 14:21:58 +0200

    q3 is done

--- Main.js
+++ Main.js
@@ -14,9 +14,41 @@ console.log(last[1, 4, 9, -2], 6) */
 
 //ex2
 
-function join(arr, n){
-    let c = arr.join(n);
-    return c;
+//approach 1
+/* function join(arr, n){
+    let c = arr.toString();
+    return c.join(n);
 }
 
-console.log(join(["Red", "Green", "White", "Black"], "+"));
+console.log(join(["Red", "Green", "White", "Black"], ",")); */
+
+//approach 2
+/* let arr =["Red", "Green", "White", "Black"]
+let oneString =arr.reduce(function(acc , item){ return acc + item +"," },'');
+alert(oneString); */
+
+//approach 3
+/* myColor = ["Red", "Green", "White", "Black"];
+console.log(myColor.toString());
+console.log(myColor.join());
+console.log(myColor.join('+')); */
+
+//ex 3
+//approach 1
+/* let a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+for(let i = 0; i < a.length - 1; i++){
+    console.log("row " + (i + 1));
+   let c = a[i].map(function(element){
+       
+    console.log(element);
+   });
+
+} */
+//approach 2
+/* const priint =a.map(function(item , index){
+console.log("row " + index);
+item.map(function(itemofindex){
+console.log(itemofindex);
+})
+})
+ */
